46dc260dfd415afe857815849e3e327732d27c05
vu <>
2022-07-18 02:04:10 +0700

    update ui

--- Mooc2022/MovieCell.swift
+++ Mooc2022/MovieCell.swift
@@ -12,15 +12,18 @@ class MovieCell: UICollectionViewCell {
 
     // MARK: Properties
     static let identifier: String = "MovieCell"
-    
+    let defaultHeightCell: CGFloat = 200
+
     // MARK: IBOutlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var containerView: UIView!
     // MARK: - View Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        configureUI()
     }
     
     override func prepareForReuse() {
@@ -31,6 +34,10 @@ class MovieCell: UICollectionViewCell {
 
 // MARK: Configure
 extension MovieCell {
+    func configureUI() {
+        containerView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner] // Top right corner, Top left corner respectively
+    }
+    
     func configureCell(model: MovieModel) {
         guard let image = model.posterPath else {
             return
